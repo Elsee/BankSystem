fbcf146250ea1d88d5038b17dd0a0c58cf89419b
Elsee <svetlana_else@mail.ru>
2016-11-23 02:44:24 +0300

    Transaction Form

--- src/ui/Transactions/TransactionFormController.java
+++ src/ui/Transactions/TransactionFormController.java
@@ -1,10 +1,12 @@
-package ui;
+package ui.Transactions;
 
 import environment.ViewController;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.*;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 import java.awt.*;
 
@@ -13,6 +15,15 @@ import java.awt.*;
  */
 
 public class TransactionFormController extends ViewController {
+    @FXML
+    ChoiceBox cb;
+    @FXML
+    TextField toAccountField;
+    @FXML
+    TextField amountField;
+    @FXML
+    Text actiontarget;
+
     ObservableList<String> accountsList = FXCollections.observableArrayList("account1", "account2");
 
     @FXML
@@ -22,15 +33,12 @@ public class TransactionFormController extends ViewController {
     }
 
     @FXML
-    ChoiceBox cb;
-
-    @FXML
     void back() {
         transitionTo("screen1");
     }
 
     @FXML
     void mainScreen() {
-        transitionTo("screen2");
+        transitionTo("customerMain");
     }
 }
