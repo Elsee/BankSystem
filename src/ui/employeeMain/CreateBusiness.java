5b75bd9ef427b15b7b4c972c571e1a9d852698bd
cubazis <grebenkin@phystech.edu>
2016-11-22 23:21:49 +0300

    #cubazis SQL Передача param в контроллеры

--- src/ui/employeeMain/CreateBusiness.java
+++ src/ui/employeeMain/CreateBusiness.java
@@ -2,11 +2,28 @@ package ui.employeeMain;
 
 import environment.ViewController;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 /**
  * Created by cubazis on 22.11.16.
  */
 public class CreateBusiness  extends ViewController {
+
+    @FXML
+    public TextField vatinField;
+    @FXML
+    public TextField regionField;
+    @FXML
+    public TextField cityField;
+    @FXML
+    public TextField streetField;
+    @FXML
+    public TextField houseField;
+    @FXML
+    public TextField moneyField;
+    @FXML
+    public TextField phoneField;
 
     public void back(ActionEvent actionEvent) {
         transitionTo("employeeMain");
--- src/ui/employeeMain/EditIndividual.java
+++ src/ui/employeeMain/EditIndividual.java
@@ -2,11 +2,44 @@ package ui.employeeMain;
 
 import environment.ViewController;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 /**
  * Created by cubazis on 22.11.16.
  */
 public class EditIndividual  extends ViewController {
+    @FXML
+    public TextField firstNameField;
+    @FXML
+    public TextField lastNameField;
+    @FXML
+    public TextField passportField;
+    @FXML
+    public TextField sexField;
+    @FXML
+    public TextField bdateField;
+    @FXML
+    public TextField vatinField;
+    @FXML
+    public TextField regionField;
+    @FXML
+    public TextField cityField;
+    @FXML
+    public TextField streetField;
+    @FXML
+    public TextField houseField;
+    @FXML
+    public TextField apartmentField;
+    @FXML
+    public TextField loginField;
+    @FXML
+    public TextField passField;
+    @FXML
+    public TextField moneyField;
+    @FXML
+    public TextField phoneField;
+
     public void back(ActionEvent actionEvent) {
         transitionTo("employeeMain");
     }
